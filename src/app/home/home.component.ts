b1fe89029777a25dc2774510a8c98f03beabfda4
debugmodedotnet <debugmode@outlook.com>
2020-07-02 18:41:48 +0530

    home page done untill new course

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HomeService} from '../shared/home.service'
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  homeData: any; 
+  constructor(private homeService: HomeService) { }
 
   ngOnInit(): void {
+
+    this.homeService.getHomeData().subscribe(
+      (data)=> {
+        this.homeData = data; 
+        console.log(this.homeData);
+      },
+      (error)=>{console.log(error)}
+    )
+
   }
 
 }
---
+++ src/app/shared/home.service.ts
@@ -0,0 +1,41 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError, observable } from 'rxjs';
+import { tap, catchError, map } from 'rxjs/operators';
+
+@Injectable({
+ providedIn:'root'
+})
+export class HomeService{
+
+     apiurl ='https://cdn.contentstack.io/v3/content_types/home_page/entries/blt93ad0d3e3f401472?environment=poc&include[]=new_courses.courses&locale=en-us&include[]=certification.certification_category&include[]=certification.certifications&include[]=instructors_section.instructor&include[]=partner_relationships_section.partner&include[]=trending_courses.course&include[]=testimonial_section.testimonial&include[]=pathway_block.pathway_block_2.pathway';
+   // apiurl = 'https://cdn.contentstack.io/v3/content_types/course/entries/?environment=poc';
+    headers = new HttpHeaders()
+    .set('Content-Type', 'application/json')
+    .set("api_key","blt88a7ab59a822374a")
+    .set("access_token","cs79ecf5e7c24502ba9da94fe0")
+    .set('Accept', 'application/json');
+  
+    httpOptions = {
+      headers: this.headers
+    };
+
+    constructor(private http: HttpClient){
+
+    }
+
+    getHomeData():Observable<any> {
+        return this.http.get<any>(this.apiurl,this.httpOptions).pipe(
+            tap(data => {console.log(data)}),
+            catchError(this.handleError)
+          );
+    }
+
+
+    private handleError(error: any) {
+        console.error(error);
+        return throwError(error);
+      }
+
+}
